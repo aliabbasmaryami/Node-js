4ea4e71e28b5ec24437350ded7e3b0bcbbd995a9
AliAbbas <aliabbasmaryami50@gmail.com>
2024-05-06 18:52:59 -0700

    server created

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const a = 20;
-const b = 10;
-const c = 20;
-console.log(a + b + c);
+const http = require("http");
+const data = require("./firstApi");
+http
+  .createServer((req, resp) => {
+    resp.writeHead(200, { "content-Type": "application/Json" });
+    resp.write(JSON.stringify());
+    resp.end();
+  })
+  .listen(6000);
+console.log("hello");
