6eb1a89a0680bc7927ede2ea0b49bc28cd24e6c8
AliAbbas <aliabbasmaryami50@gmail.com>
2024-06-21 14:36:49 -0700

    new project

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
-// This is called parsers
-
+const fs = require("fs");
+const { isUtf8 } = require("buffer");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("index");
+  const filesDirectory = path.join(__dirname, "files");
+  fs.readdir(filesDirectory, function (err, files) {
+    res.render("index", { files: files });
+  });
 });
-app.get("/profile/:username", function (req, res) {
-  res.send(`welocme,${req.params.username}`);
+
+app.get("/files/:filename", function (req, res) {
+  fs.readFile(
+    `../files/${req.params.filename}`,
+    "utf-8",
+    function (err, filedata) {
+      res.render("show", {
+        filename: req.params.filename,
+        filedata: filedata,
+      });
+    }
+  );
 });
-app.get("/profile/:username/:age", function (req, res) {
-  res.send(`welocme,${req.params.username} of age ${req.params.age}`);
+
+app.post("/create", function (req, res) {
+  const fileName = req.body.title.split(" ").join("") + ".txt";
+  const filePath = path.join(__dirname, "files", fileName);
+  fs.writeFile(filePath, req.body.details, function (err) {
+    res.redirect("/");
+  });
 });
 
 app.listen(3000, function () {
-  console.log("port 3000");
+  console.log("Server is running on port 3000");
 });
