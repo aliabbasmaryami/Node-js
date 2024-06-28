be5aff624c02ed8029414596e2c8dbf6630f545b
AliAbbas <aliabbasmaryami50@gmail.com>
2024-06-27 18:08:19 -0700

    curd operations

--- index.js
+++ index.js
@@ -1,89 +1,41 @@
-// const express = require("express");
-// const app = express();
-// const path = require("path");
-// const fs = require("fs");
-// const { isUtf8 } = require("buffer");
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
-// app.set("view engine", "ejs");
-
-// app.get("/", function (req, res) {
-//   const filesDirectory = path.join(__dirname, "files");
-//   fs.readdir(filesDirectory, function (err, files) {
-//     res.render("index", { files: files });
-//   });
-// });
-
-// app.get("/files/:filename", function (req, res) {
-//   fs.readFile(
-//     `../files/${req.params.filename}`,
-//     "utf-8",
-//     function (err, filedata) {
-//       res.render("show", {
-//         filename: req.params.filename,
-//         filedata: filedata,
-//       });
-//     }
-//   );
-// });
-
-// app.post("/create", function (req, res) {
-//   const fileName = req.body.title.split(" ").join("") + ".txt";
-//   const filePath = path.join(__dirname, "files", fileName);
-//   fs.writeFile(filePath, req.body.details, function (err) {
-//     res.redirect("/");
-//   });
-// });
-
-// app.listen(3000, function () {
-//   console.log("Server is running on port 3000");
-// });
-
 const express = require("express");
 const app = express();
+const path = require("path");
 
-const userModal = require("./views/usersmodal");
+const userModel = require("./models/user");
 
-app.get("/", function (req, res) {
-  res.send("Heyy!");
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-// created users
-app.get("/create", async (req, res) => {
-  let createduser = await userModal.create({
-    name: "ali",
-    username: "amir",
-    email: "ali@gmail.com",
+app.get("/read", async (req, res) => {
+  let users = await userModel.find();
+  res.render("read", { users });
+});
+
+app.post("/create", async (req, res) => {
+  let { name } = req.body;
+  let { email } = req.body;
+  let { image } = req.body;
+  let createdUser = await userModel.create({
+    name,
+    email,
+    image,
   });
-  res.send(createduser);
+  res.send(createdUser);
 });
-
-// updated userModal
-app.get("/update", async (req, res) => {
-  let updateduser = await userModal.findOneAndUpdate(
-    { username: "aliabbas" },
-    { name: "amir mir" },
-    { new: true }
-  );
-  res.send(updateduser);
+app.get("/delete/:id", async (req, res) => {
+  try {
+    let deletedUser = await userModel.findOneAndDelete({ _id: req.params.id });
+    res.redirect("/read");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error deleting user");
+  }
 });
-
-// read users
-
-app.use("/read", async (req, res) => {
-  let users = await userModal.find();
-  res.send(users);
-});
-
-// deleted users
-
-app.use("/delete", async (req, res) => {
-  let deletedUsers = await userModal.findOneAndDelete(
-    { name: "Gaurav" },
-    { username: "ali" }
-  );
-  res.send(deletedUsers);
-});
-console.log("alaa");
 app.listen(3000);
--- models/user.js
+++ models/user.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-
-mongoose.connect(`mongodb://127.0.0.1:27017/mongopractics`);
+mongoose.connect("mongodb://127.0.0.1:27017/testapp");
 
 const userSchema = mongoose.Schema({
   name: String,
-  username: String,
+  image: String,
   email: String,
 });
+
 module.exports = mongoose.model("user", userSchema);
